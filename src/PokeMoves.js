a12ae7322453fcccc5c7c4f39e4c65b65935aadc
Alexander Feinberg <feinberg.alexander@gmail.com>
2022-10-10 03:02:22 +0000

    Update 4 files

---
+++ src/PokeMoves.js
@@ -0,0 +1,19 @@
+import { moves } from "./data";
+import PokeMoveCard from "./PokeMoveCard";
+
+function PokeMoves() {
+  return (
+    <div>
+      <h1>PokeMoves</h1>
+      <ul>
+        {moves.map((item) => (
+          <PokeMoveCard key={item.id} {...item}>
+            {item.id}. {item.move}
+          </PokeMoveCard>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PokeMoves;
